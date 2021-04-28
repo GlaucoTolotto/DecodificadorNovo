8a494369205dbedf25d7b44be5cf7a48e6be62d9
GlaucoTolotto <glaucotolotto@gmail.com>
2021-04-28 15:59:02 -0300

    css implementado tentativa de fazer o incremento desaprecer quando selecionado o Base64

--- decodificador.js
+++ decodificador.js
@@ -1,53 +1,67 @@
 function codificaGeral() {
-    var baseSelecionada = document.querySelector("#selecionar").selectedIndex
-    var resultado = document.getElementById("retorno")
-   if (baseSelecionada == 0){
-        var mensagem = document.getElementById("texto").value
-        if (document.getElementById("codificar").checked){
-          resultado.innerHTML = btoa(mensagem)
+    var baseSelecionada = document.querySelector("#selecionar").selectedIndex;
+    var resultado = document.getElementById("retorno");
+    if (baseSelecionada == 0) {
+        var mensagem = document.getElementById("texto").value;
+        if (document.getElementById("codificar").checked) {
+            resultado.innerHTML = btoa(mensagem);
         }
         else {
-            resultado.innerHTML = atob(mensagem)
+            resultado.innerHTML = atob(mensagem);
         }
     }
-    
-    
-    else{
-       var mensagem = document.getElementById("texto").value;
-       var resultado = document.getElementById("retorno");
+
+
+    else {
+        var mensagem = document.getElementById("texto").value;
+        var resultado = document.getElementById("retorno");
         var chave = parseInt(document.getElementById("incremento").value);
-        
-        if(document.getElementById("codificar")){
-            resultado.innerHTML = codificaCifra(mensagem,chave)
+        if (document.getElementById("codificar")) {
+            resultado.innerHTML = codificaCifra(mensagem, chave);
         }
-        else{
-            resultado.innerHTML = decodificaCifra(mensagem,chave)
+        else {
+            resultado.innerHTML = decodificaCifra(mensagem, chave);
         }
 
-   }
-   
-   
+    }
+
+
 }
 
-function codificaCifra (string, chave){
+// function mostrarIncr (){
+//     var baseSelecionada = document.querySelector("#selecionar").selectedIndex;
+//     if (baseSelecionada == 1){
+//         var incremento = document.querySelector(".containerIncremento");
+//         incremento.style.display = "flex"
+//         incremento.style.flexDirection = "column"
+//         incremento.style.alignItems = "center"
+//     }
+//     else {
+//         var incremento = document.querySelector(".containerIncremento");
+//         incremento.style.display = "none"
+
+//     }
+// }
+
+function codificaCifra(string, chave) {
     var result = [];
     var lista = string.split("");
-    for (var i=0; i <lista.length; i++){
+    for (var i = 0; i < lista.length; i++) {
         var codigo = lista[i].charCodeAt();
-        var resultado = ((codigo-97+chave)%26)+97;
+        var resultado = ((codigo - 97 + chave) % 26) + 97;
         result.push(String.fromCharCode(resultado));
     }
     return result.join("");
 }
 
-function decodificaCifra (string, chave) {
+function decodificaCifra(string, chave) {
     var result = [];
     var lista = string.split("");
-    for (var i=0; i < lista.length; i++){
+    for (var i = 0; i < lista.length; i++) {
         var codigo = lista[i].charCodeAt();
-        
-        if ((codigo-97-chave)<0)var resultado = ((codigo-97-chave+26)%26)+97;
-        else var resultado = ((codigo-97-chave)%26)+97;
+
+        if ((codigo - 97 - chave) < 0) var resultado = ((codigo - 97 - chave + 26) % 26) + 97;
+        else var resultado = ((codigo - 97 - chave) % 26) + 97;
 
         result.push(String.fromCharCode(resultado));
     }
